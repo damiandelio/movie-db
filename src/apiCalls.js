076edc04f52fb151d8732d99dc4a80375b680a88
Damián De Lio <damiandelio@gmail.com>
2020-06-15 14:43:11 -0300

    add infinite scroll to movies

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,17 @@
 import CONSTANTS from "./constants";
 
-export const GET_DISCOVER_MOVIES_CFG = {
-  method: "get",
-  url: CONSTANTS.URL_DISCOVER_MOVIES,
-  headers: {},
-  params: {
-    api_key: "a7a0795aaec83387554c7a0f2c80c09a",
-    language: "en-US",
-    sort_by: "popularity.desc",
-    include_adult: "false",
-    include_video: "false",
-    page: "1",
-  },
-};
+export function GET_DISCOVER_MOVIES_CFG(targetPage) {
+  return {
+    method: "get",
+    url: CONSTANTS.URL_DISCOVER_MOVIES,
+    headers: {},
+    params: {
+      api_key: "a7a0795aaec83387554c7a0f2c80c09a",
+      language: "en-US",
+      sort_by: "popularity.desc",
+      include_adult: "false",
+      include_video: "false",
+      page: targetPage,
+    },
+  };
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,32 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import MovieItem from '../../components/MovieItem/MovieItem'
 import styles from './HomePage.module.scss'
 import axios from "axios";
 import { GET_DISCOVER_MOVIES_CFG } from '../../apiCalls'
+import InfiniteScroll from 'react-infinite-scroller';
 
 const HomePage = () => {
 
     const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        axios(GET_DISCOVER_MOVIES_CFG)
+    let loadFunc = (page) => {
+        console.log('page: ' + page)
+        axios(GET_DISCOVER_MOVIES_CFG(page))
             .then(function (response) {
-                // console.log(response.data)
-                setMovies(response.data.results);
+                setMovies([...movies, ...response.data.results]);
             })
             .catch(function (error) {
                 console.log(error);
             });
-    }, []);
+    }
 
     return (
         <React.Fragment>
             <main className={styles.mainContainer}>
-                <ul className={styles.moviesContainer}>
-                    {movies.map((movie) => (
-                        <MovieItem movie={movie} key={movie.id} />
-                    ))}
-                </ul>
+                <InfiniteScroll
+                    pageStart={490}
+                    loadMore={loadFunc}
+                    hasMore={true}
+                    loader={<div className="loader" key={0}>Loading ...</div>}
+                >
+                    <ul className={styles.moviesContainer}>
+                        {movies.map((movie) => (
+                            <MovieItem movie={movie} key={movie.id} />
+                        ))}
+                    </ul>
+                </InfiniteScroll>
             </main>
         </React.Fragment>
     )
