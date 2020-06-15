799729d7688e97cfb09540351757851cb847127e
Damián De Lio <damiandelio@gmail.com>
2020-06-15 13:39:57 -0300

    updates

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const URL_BASE = 'https://api.themoviedb.org/3'
 
 const CONSTANTS = {
-    URL_DISCOVER_MOVIES: URL_BASE+'/discover/movie',
+    URL_DISCOVER_MOVIES: URL_BASE + '/discover/movie',
     URL_IMG_BASE: 'https://image.tmdb.org/t/p/w500'
 }
 
