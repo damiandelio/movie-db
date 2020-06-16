9365c612ab23b6b5870760c9db4a213951185e6f
Damián De Lio <damiandelio@gmail.com>
2020-06-16 17:19:25 -0300

    add a new page - not found 404

--- src/route.js
+++ src/route.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 // Components
 import HomePage from "./pages/HomePage/HomePage";
-
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 const router = (
-    <Router>
-        <Route path="/" component={HomePage}/>
-    </Router>
+  <Router>
+    <Route exact path="/" component={HomePage} />
+    <Route component={NotFoundPage} />
+  </Router>
 );
 
 export default router;
